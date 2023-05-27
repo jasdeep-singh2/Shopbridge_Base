f3ef3df0a5b611556ea33437023deb9c9aab0720
jasdeep singh <jasdeepsinghcyb@gmail.com>
2023-05-27 12:15:13 +0530

    Adding Product Controller

---
+++ ShopBridge/WebAPI/Controllers/BaseAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseAPIController : ControllerBase
+    {
+        
+    }
+}
---
+++ ShopBridge/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace WebAPI.Controllers
+{
+    public class ProductsController : BaseAPIController
+    {
+        private readonly DataContext _dataContext;
+        public ProductsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _dataContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(Guid id)
+        {
+            return await _dataContext.Products.FindAsync(id);
+        }
+
+    }
+}

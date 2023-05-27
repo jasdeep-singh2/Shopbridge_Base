849ed902ba5385d067fe57e39f3ab5821bf7eb95
jasdeep singh <jasdeepsinghcyb@gmail.com>
2023-05-27 11:52:35 +0530

    Seetin up project

---
+++ ShopBridge/Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ShopBridge/Persistence/Seed.cs
@@ -0,0 +1,76 @@
+﻿using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Mobile",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                },
+                 new Product
+                {
+                    Name = "Television",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "SmartPhone",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Alexa",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Monitor",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Laptop",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Airpods",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Earbuds",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Speakers",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Monitor",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }, new Product
+                {
+                    Name = "Mouse",
+                    Description = "Electronic Item",
+                    Date = DateTime.Now,
+                }
+            };
+
+            await context.Products.AddRangeAsync(products);
+            await context.SaveChangesAsync();
+        }
+    }
+}
